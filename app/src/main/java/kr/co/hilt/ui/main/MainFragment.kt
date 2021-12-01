308953c263f7ca533e90d6b091e04626ff72ab18
philip <philip@taeha.co.kr>
2021-12-01 16:30:13 +0900

    hilt

--- app/src/main/java/kr/co/hilt/ui/main/MainFragment.kt
+++ app/src/main/java/kr/co/hilt/ui/main/MainFragment.kt
@@ -2,18 +2,24 @@ package kr.co.hilt.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
 import kr.co.hilt.R
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
+    var TAG = javaClass.simpleName
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
+    @Inject
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
@@ -25,7 +31,9 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.name.value = "kp"
+        Log.e(TAG, viewModel.name.value as String);
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/kr/co/hilt/ui/main/MainViewModel.kt
+++ app/src/main/java/kr/co/hilt/ui/main/MainViewModel.kt
@@ -1,7 +1,10 @@
 package kr.co.hilt.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    lateinit var name: MutableLiveData<Any>
 }
